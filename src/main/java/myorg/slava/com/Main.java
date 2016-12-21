238c87b37be5b2dda874c8345b217756134a49e5
v.seregin <konnley777@yandex.ru>
2016-12-21 15:14:05 +0300

    add Main

---
+++ src/main/java/myorg/slava/com/Main.java
@@ -0,0 +1,11 @@
+package myorg.slava.com;
+
+/**
+ * v.seregin
+ * on 21.12.2016.
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hi");
+    }
+}
