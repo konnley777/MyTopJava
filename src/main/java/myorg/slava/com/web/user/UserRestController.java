c5f819c41cd25c21c1a61c016b11d86317234e5d
konnley777 <konnley777@yandex.ru>
2017-01-02 11:25:24 +0300

    UserRestController give him body

--- src/main/java/myorg/slava/com/web/user/UserRestController.java
+++ src/main/java/myorg/slava/com/web/user/UserRestController.java
@@ -1,10 +1,31 @@
 package myorg.slava.com.web.user;
 
+import myorg.slava.com.LoggedUser;
+import myorg.slava.com.LoggerWrapper;
+import myorg.slava.com.model.User;
 import myorg.slava.com.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 
 @Controller
 public class UserRestController {
+    private static final LoggerWrapper LOG = LoggerWrapper.get(UserRestController.class);
    @Autowired
-    private UserService service;}
+    private UserService service;
+
+public User get() {
+int id = LoggedUser.getId();
+ LOG.info("get some logging user");
+    return service.get(id);
+}
+    public void delete() {
+        int id = LoggedUser.getId();
+        LOG.info("delete some logging user");
+         service.delete(id);
+    }
+    public void update(User user) {
+        int id = LoggedUser.getId();
+        LOG.info("update some logging user");
+        service.update(user);
+    }
+}
